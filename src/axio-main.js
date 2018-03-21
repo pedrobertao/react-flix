7705ab5b7766de30bbd247180005f93581f7d0a7
Pedro Bertao <pedrobertao11@hotmail.com>
2018-03-21 11:18:36 -0300

    Limpando

--- src/axio-main.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-import { API_SEARCH } from './utils/constants';
-const instance = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/'
-});
-
-export default instance;
